0c9e60ed09dc4d35845af82c109c685d4f3fc77b
Tim Musson <tim@troop2el.org>
2026-03-09 20:08:41 -0400

    fleshing out to test things

---
+++ hamburger-menu/distribute.bat
@@ -0,0 +1,14 @@
+md eagle
+md reports
+md venturing
+md laurels
+md about
+md contact
+
+copy index.html eagle
+copy index.html reports
+copy index.html venturing
+copy index.html laurels
+copy index.html about
+copy index.html contact
+
---
+++ hamburger-menu/reports/location.js
@@ -0,0 +1,5 @@
+<!-- hide script from old browsers
+document.write( "<br><br><h2><center> Reports </center></h2>" )
+document.write( "<hr>" )
+// end hiding script from old browsers -->
+
