33d53973f5496c3fd3f78c4ebac02042aa40d7f0
Tim Musson <tim@troop2el.org>
2026-03-10 19:04:36 -0400

    playing with the menu

--- hamburger-menu/distribute.bat
+++ hamburger-menu/distribute.bat
@@ -1,14 +1,18 @@
 md eagle
+md MBCinfo
 md reports
-md venturing
 md laurels
+md calendar
+md venturing
 md about
 md contact
 
 copy index.html eagle
+copy index.html MBCinfo
 copy index.html reports
-copy index.html venturing
 copy index.html laurels
+copy index.html calendar
+copy index.html venturing
 copy index.html about
 copy index.html contact
 
