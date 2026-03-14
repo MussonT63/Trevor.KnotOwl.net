622443946cb35f5baa3a8ba112a1ef70727f0ddc
Tim Musson <tim@troop2el.org>
2026-03-13 21:10:41 -0400

    more menu tweeking

--- hamburger-menu/location.js
+++ hamburger-menu/location.js
@@ -1,5 +1,5 @@
 <!-- hide script from old browsers
-document.write( "<br><br><h2><center> Home, 600px threshold </center></h2>" )
+document.write( "<br><br><h2><center> Home, 400px threshold </center></h2>" )
 document.write( "<hr>" )
 // end hiding script from old browsers -->
 
